569daefa9f5d03d4471e2ed6804d331c2c581d8d
303-12\user <>
2023-01-10 21:50:01 +0900

    [유상훈] -유상훈

--- src/main/java/com/agora12/test/SpringTest/model/model.java
+++ src/main/java/com/agora12/test/SpringTest/model/model.java
@@ -2,4 +2,29 @@ package com.agora12.test.SpringTest.model;
 
 public class model {
 
+	private String introduce;
+	private String user;
+	private String content;
+	public String getIntroduce() {
+		return introduce;
+	}
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+	
+	
 }
